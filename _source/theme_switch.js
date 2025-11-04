0f30a7de787ef42eda781e2408f6948764e56e91
DaPakoREE <dapakoreeee@gmail.com>
2025-11-04 09:47:23 +0100

    Theme toggle mostly complete

--- _source/theme_switch.js
+++ _source/theme_switch.js
@@ -1,21 +1,40 @@
 const page = document.querySelector("html");
 const themeToggle = document.getElementById("theme-toggle");
 const themeSwitch = themeToggle.querySelector("#theme-switch");
-const themeSwitchLabel = themeToggle.querySelector("label");
+
+let themeSwitchLabelImage = themeToggle.querySelector("label .svg");
+let SVGS = []
+let responsiveSVGS = []
 
 let lightMode = false
+let previousFileNameAddon = "light"
 let fileNameAddon = "dark"
+
+function getResponsiveSVGS(svg) {
+    return (svg.src.indexOf("light") > -1)
+}
 
 function switchTheme() {
     lightMode = !lightMode
     if (lightMode) {
         page.classList.add("light")
-        fileNameAddon = "light"
+        themeSwitchLabelImage.src = themeSwitchLabelImage.src.replace("sun", "moon")
+        fileNameAddon = "dark"
     } else {
         page.classList.remove("light")
-        fileNameAddon = "dark"
+        themeSwitchLabelImage.src = themeSwitchLabelImage.src.replace("moon", "sun")
+        fileNameAddon = "light"
     }
+    responsiveSVGS.forEach(svg => {
+        svg.src = svg.src.replace(previousFileNameAddon, fileNameAddon)
+    });
+    previousFileNameAddon = fileNameAddon
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    SVGS = document.querySelectorAll(".svg");
+    responsiveSVGS = Array.from(SVGS).filter(getResponsiveSVGS);
+});
 
 themeSwitch.addEventListener("click", () => {
     switchTheme()
