020e0c799e5f883c1a01a648a95fd38b9f6829db
DaPakoREE <dapakoreeee@gmail.com>
2025-11-07 16:01:56 +0100

    Continued on the revamp

--- _source/category_switch.js
+++ _source/category_switch.js
@@ -5,6 +5,16 @@ const previousButton = document.getElementById("arrow-previous")
 let selectedCategory
 let selectedCategoryIndex
 
+
+function updateSelectedCategory() {
+    console.log(selectedCategory)
+    selectedCategory.classList.remove("active-category")
+    selectedCategory = projectCategories[selectedCategoryIndex]
+    console.log(selectedCategory)
+    selectedCategory.classList.add("active-category")
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
     selectedCategoryIndex = 0
     selectedCategory = projectCategories[selectedCategoryIndex]
@@ -12,7 +22,19 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 nextButton.addEventListener("click", () => {
-    selectedCategoryIndex += 1
-    selectedCategory = projectCategories[selectedCategoryIndex]
-    console.log(selectedCategory)
+    if (selectedCategoryIndex == projectCategories.length - 1) {
+        selectedCategoryIndex = 0
+    } else {
+        selectedCategoryIndex += 1
+    }
+    updateSelectedCategory()
+})
+
+previousButton.addEventListener("click", () => {
+    if (selectedCategoryIndex == 0) {
+        selectedCategoryIndex = projectCategories.length - 1
+    } else {
+        selectedCategoryIndex -= 1
+    }
+    updateSelectedCategory()
 })
